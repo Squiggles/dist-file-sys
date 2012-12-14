fc74caa5111064f6915a342fdacce3385cbad9a4
Squiggles <obrienk8@tcd.ie>
2012-12-14 08:47:53 +0000

    Renamed FileSystem as Network because it starts multiple file systems

---
+++ Network.py
@@ -0,0 +1,119 @@
+import Pyro4
+import os
+import sys
+
+import CurrentDirectory as CD
+
+# Class representing the file system 
+class FileSystem(object):
+    
+    # Each system has a name and a private directory
+    def __init__(self,n):
+        self.system = n
+        self.name = n + 'files'
+        self.path = self.name + '/'
+        if not n == 'backup':
+            self.backup = Pyro4.Proxy("PYRONAME:filesystem.backup")
+            self.bpath = 'backupfiles/'
+            print n + ' reporting!'
+        
+    # Build a representative directory structure for the directory service
+    def buildDir(self):
+        top = _buildDir(self.name,self.path)
+        top.system = self.system
+        return top
+
+    # Build a mapping between filename and (system,path)
+    def listFiles(self):
+        print 'Servicing request for file dictionary'
+        return _listFiles(self.path,self.path)
+
+    # Read from the specified file and return the contents
+    def readFile(self, path):
+        try:
+            f = open(path,'r')
+            print "Read @ " + path
+            return f.read()
+        except IOError as e:
+            print 'Read attempt on nonexistant file @ ' + path
+            #return 'ERROR: file doesn\'t exist'
+            return None
+            
+    def readLine(self, path, i):
+        try:
+            f = open(path,'r')
+            contents = f.read().split('\n')
+            print "Read @ " + path
+            if i < len(contents):
+                return contents[i]
+            else:
+                return None
+        except IOError as e:
+            print 'Read attempt on nonexistant file @ ' + path
+            return None
+
+    #TODO: tell replicator to write backup
+    # Create a new file or overwrite the previous, inserting the supplied text.
+    def writeFile(self, path, text):
+        if not self.system == 'backup':
+            self.backup.writeFile(self.bpath+path,text)
+        f = open(path,'w+')
+        f.write(text)
+        f.close()
+        print "Write @ " + path + "."
+
+    # Append text to the end of the specified file
+    def appendFile(self, path, text):
+        try:
+            if not system == 'backup':
+                self.backup.writeFile(bpath.self+path,text)
+            f = open(path, 'a')
+            f.write('\n' + text)
+            f.close()
+            print "Append @ " + path
+        except IOError as e:
+            print "Append attempt on nonexistant file @ " + path
+            return None
+
+# Recursively builds dictionary of name to (serverID,path) pairs
+def _listFiles(name,path):
+    files = {}
+    for item in os.listdir(path):
+        addr = os.path.join(path,item)
+        if os.path.isfile(addr):
+            # Enter new (key,value) pair
+            files[item] = (name,addr)
+        else:
+            # Recurse and integrate results with existing dictionary
+            files = dict(files.items() + _listFiles(name,addr).items())
+    return files
+
+# Build a representative directory structure for the directory service
+def _buildDir(name,path):
+    curry = CD.CurrentDirectory(name,path)
+        
+    for item in os.listdir(path):
+        addr = os.path.join(path,item)
+
+        if os.path.isfile(addr):
+            curry.addFile(item)
+        elif os.path.isdir(addr):
+            curry.addDir(_buildDir(item, path + item + "/"))
+                
+    return curry
+
+coo = Pyro4.Proxy('PYRONAME:coordinator')
+systems = coo.getSystems()
+systems.insert(0,'backup')
+
+# Start servers
+daemon = Pyro4.Daemon()             # make Pyro daemon
+ns = Pyro4.locateNS()               # find name server
+for fs in systems:
+    filesystem = FileSystem(fs)
+    uri = daemon.register(filesystem)       # register filesystem
+    ns.register('filesystem.'+fs, uri)      # register with name in name server
+    print 'filesystem.' + fs + ' ready'
+
+
+daemon.requestLoop()                # wait
